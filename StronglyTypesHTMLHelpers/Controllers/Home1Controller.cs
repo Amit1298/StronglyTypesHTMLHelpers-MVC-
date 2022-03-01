48371c0dad2fca68915d8fa24967e61446371064
Amit <amitkr24031995@gmail.com>
2022-03-01 22:41:05 +0530

    Adding StronglyTypesHTMLHelpers

---
+++ StronglyTypesHTMLHelpers/Controllers/Home1Controller.cs
@@ -0,0 +1,23 @@
+﻿using StronglyTypesHTMLHelpers.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StronglyTypesHTMLHelpers.Controllers
+{
+    public class Home1Controller : Controller
+    {
+        // GET: Home1
+        [HttpPost]
+        public ActionResult Index(Calculation c)
+        {
+            int num1 = c.num1;
+            int num2 = c.num2;
+            int result = num1 + num2;
+            ViewBag.result = result;
+            return View();
+        }
+    }
+}
